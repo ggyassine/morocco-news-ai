a8339461c478b24a68420f04554b8f3cdf267839
ggyassine <yassinehamini9@gmail.com>
2026-09-16 22:17:58 +0100

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,51 +1,231 @@
-# Sources are deliberately separated by trust tier.
-# RSS URLs should be verified in the deployment environment. If a source changes its feed URL,
-# update it here without changing the rest of the application.
+# Arabic Moroccan news sources only.
+#
+# Trust:
+# A = Official / primary source
+# B = Established Moroccan news media
+# C = Sports / specialist media
+
+
 SOURCES = [
-    # Official / primary
-    ('MAP', 'https://www.mapnews.ma/en/rss', 'A'),
-    ('SNRTnews', 'https://snrtnews.com/rss.xml', 'A'),
-    # Moroccan press
-    ('Hespress', 'https://www.hespress.com/feed', 'B'),
-    ('Le360', 'https://fr.le360.ma/rss', 'B'),
-    ('Médias24', 'https://medias24.com/feed/', 'B'),
-    ('TelQuel', 'https://telquel.ma/feed/', 'B'),
-    ('Aujourd’hui Le Maroc', 'https://aujourdhui.ma/feed', 'B'),
-    ('L’Economiste', 'https://www.leconomiste.com/rss.xml', 'B'),
-    # Sports / transfer reporting
-    ('Elbotola', 'https://m.elbotola.com/fr/rss/', 'B'),
-    ('Foot Mercato', 'https://www.footmercato.net/rss', 'B'),
-    ('ESPN Soccer', 'https://www.espn.com/espn/rss/soccer/news', 'B'),
-    ('Al Jazeera Sports', 'https://www.aljazeera.com/xml/rss/all.xml', 'B'),
-    ('Reuters Sports', 'https://feeds.reuters.com/reuters/sportsNews', 'A'),
+
+    # =========================
+    # Official Moroccan sources
+    # =========================
+
+    ('MAP عربي', 'https://www.mapnews.ma/ar/', 'A'),
+
+    ('SNRTnews عربي', 'https://snrtnews.com/ar/', 'A'),
+
+
+    # =========================
+    # Major Moroccan Arabic media
+    # =========================
+
+    ('هسبريس', 'https://www.hespress.com/', 'B'),
+
+    ('Le360 عربي', 'https://ar.le360.ma/', 'B'),
+
+    ('العمق المغربي', 'https://al3omk.com/', 'B'),
+
+    ('اليوم24', 'https://www.alyaoum24.com/', 'B'),
+
+    ('أخبارنا المغربية', 'https://www.akhbarona.com/', 'B'),
+
+    ('هبة بريس', 'https://ar.hibapress.com/', 'B'),
+
+    ('برلمان', 'https://www.barlamane.com/', 'B'),
+
+    ('كود', 'https://www.goud.ma/', 'B'),
+
+    ('كيفاش', 'https://kifache.com/', 'B'),
+
+    ('فبراير', 'https://febrayer.com/', 'B'),
+
+
+    # =========================
+    # Moroccan sports media
+    # =========================
+
+    ('البطولة', 'https://www.elbotola.com/ar/', 'C'),
+
+    ('المنتخب', 'https://www.almountakhab.com/', 'C'),
 ]
 
-# Search terms used only as a relevance filter. The system does not treat a keyword match as proof.
+
+# =========================================================
+# Morocco relevance terms
+# =========================================================
+
 MOROCCO_TERMS = [
-    'morocco','moroccan','maroc','marocain','marocaine','royaume du maroc',
-    'المغرب','المغربي','المغربية','المغاربة','المملكة المغربية','الرباط','الدار البيضاء',
-    'morocco government','morocco ministry'
+
+    'المغرب',
+    'المغربي',
+    'المغربية',
+    'المملكة المغربية',
+
+    'الرباط',
+    'الدار البيضاء',
+    'مراكش',
+    'طنجة',
+    'فاس',
+    'أكادير',
+    'وجدة',
+    'تطوان',
+    'القنيطرة',
+    'سلا',
+    'مكناس',
+    'الجديدة',
+    'الناظور',
+    'الحسيمة',
+    'بني ملال',
+    'خريبكة',
+    'آسفي',
+    'الصويرة',
+    'ورزازات',
+    'العيون',
+    'الداخلة',
+
+    'المنتخب المغربي',
+    'المنتخب الوطني',
+    'أسود الأطلس',
+    'الجامعة الملكية المغربية لكرة القدم',
 ]
+
+
+# =========================================================
+# Transfer terms
+# =========================================================
 
 TRANSFER_TERMS = [
-    'transfer','transfers','transfert','transferts','mercato','signed','signs','joins',
-    'loan','loaned','contract','renewal','renew','deal','agreement','bid','offer',
-    'interest','negotiations','medical','free agent','release','departure',
-    'انتقال','انتقالات','ميركاتو','صفقة','تعاقد','إعارة','تجديد','عرض','مفاوضات','اهتمام',
-    'اتفاق','فحص طبي','فسخ العقد','وكيل','لاعب مغربي','دولي مغربي'
+
+    'انتقال',
+    'انتقالات',
+    'صفقة',
+    'صفقات',
+    'إعارة',
+    'إعارات',
+    'تجديد',
+    'عقد',
+    'عقود',
+    'رحيل',
+    'مغادرة',
+
+    'اهتمام',
+    'مفاوضات',
+    'عرض',
+    'عروض',
+    'اتفاق',
+    'اتفاق مبدئي',
+
+    'توقيع',
+    'يوقع',
+    'وقع',
+    'ينضم',
+    'انضم',
+    'ضم',
+
+    'فحص طبي',
+    'وكيل أعمال',
+    'وكيل اللاعب',
+    'فسخ العقد',
+    'كسر العقد',
+
+    'لاعب مغربي',
+    'دولي مغربي',
+    'اللاعب المغربي',
 ]
 
-# Names are aliases, not evidence of nationality. Extend this list as players emerge.
+
+# =========================================================
+# Moroccan players
+# =========================================================
+
 MOROCCAN_PLAYERS = [
-    'Achraf Hakimi','أشرف حكيمي','Brahim Diaz','براهيم دياز','Sofyan Amrabat','سفيان أمرابط',
-    'Noussair Mazraoui','نصير مزراوي','Nayef Aguerd','نايف أكرد','Yassine Bounou','ياسين بونو',
-    'Eliesse Ben Seghir','إلياس بن صغير','Bilal El Khannouss','بلال الخنوس','Ismael Saibari','إسماعيل الصيباري',
-    'Azzedine Ounahi','عز الدين أوناحي','Sofiane Boufal','سفيان بوفال','Ayoub El Kaabi','أيوب الكعبي',
-    'Youssef En-Nesyri','يوسف النصيري','Amine Harit','أمين حارث','Abde Ezzalzouli','عبد الصمد الزلزولي',
-    'Zakaria Aboukhlal','زكرياء أبو خلال','Chadi Riad','شادي رياض','Abdelhamid Ait Boudlal','عبد الحميد آيت بودلال',
-    'Oussama Targhalline','أسامة ترغالين','Ismael Kandouss','إسماعيل قندوس','Ilias Chair','إلياس شاعر',
-    'Amine Adli','أمين عدلي','Oussama Idrissi','أسامة الإدريسي','Munir El Haddadi','منير الحدادي',
-    'Jawad El Yamiq','جواد الياميق','Sofyan El Krouni','سفيان الكرواني','Souffian El Karouani','سفيان الكرواني',
-    'Ayoub Bouaddi','أيوب بوعدي','Issa Diop','إسّا ديوب','Taha Majni','طه مجني',
-    'Anass Salah-Eddine','أنس صلاح الدين','Ismael Saibari','إسماعيل الصيباري'
+
+    'أشرف حكيمي',
+    'Achraf Hakimi',
+
+    'إبراهيم دياز',
+    'Brahim Diaz',
+
+    'سفيان أمرابط',
+    'Sofyan Amrabat',
+
+    'نصير مزراوي',
+    'Noussair Mazraoui',
+
+    'نايف أكرد',
+    'Nayef Aguerd',
+
+    'ياسين بونو',
+    'Yassine Bounou',
+
+    'إلياس بن صغير',
+    'Eliess Ben Seghir',
+
+    'بلال الخنوس',
+    'Bilal El Khannouss',
+
+    'إسماعيل الصيباري',
+    'Ismael Saibari',
+
+    'عز الدين أوناحي',
+    'Azzedine Ounahi',
+
+    'سفيان بوفال',
+    'Sofiane Boufal',
+
+    'أيوب الكعبي',
+    'Ayoub El Kaabi',
+
+    'يوسف النصيري',
+    'Youssef En-Nesyri',
+
+    'أمين حارث',
+    'Amine Harit',
+
+    'عبد الصمد الزلزولي',
+    'Abde Ezzalzouli',
+
+    'زكرياء أبو خلال',
+    'Zouhair Aboukhlal',
+
+    'شادي رياض',
+    'Chadi Riad',
+
+    'عبد الحميد آيت بودلال',
+    'Abdelhamid Ait Boudlal',
+
+    'أسامة ترغالين',
+    'Oussama Targhalline',
+
+    'إسماعيل قندوس',
+    'Ismael Kandouss',
+
+    'إلياس شاعر',
+    'Ilias Chair',
+
+    'أمين عدلي',
+    'Amine Adli',
+
+    'أسامة الإدريسي',
+    'Oussama Idrissi',
+
+    'منير الحدادي',
+    'Munir El Haddadi',
+
+    'جواد الياميق',
+    'Jawad El Yamiq',
+
+    'سفيان الكرواني',
+    'Souffian El Karouani',
+
+    'أيوب بوعدي',
+    'Ayoub Bouaddi',
+
+    'أنس صلاح الدين',
+    'Anass Salah-Eddine',
+
+    'طه مجني',
+    'Taha Majni',
 ]
