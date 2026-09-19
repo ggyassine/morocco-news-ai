119a787d65f6a7be0db816065f4ce189440babaf
ggyassine <yassinehamini9@gmail.com>
2026-09-19 12:09:24 +0100

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,408 +1,244 @@
-# =========================================================
-# MOROCCO NEWS AI
-# NEWS SOURCES
-# =========================================================
+MOROCCO_TERMS = [
+    "المغرب",
+    "المغربية",
+    "مغربي",
+    "مغاربة",
+    "الرباط",
+    "الدار البيضاء",
+    "مراكش",
+    "طنجة",
+    "فاس",
+    "أكادير",
+    "الحكومة المغربية",
+    "البرلمان المغربي",
+    "المنتخب المغربي",
+    "أسود الأطلس",
+    "محمد السادس",
+]
 
-# Trust levels:
-#
-# A = Official / primary / major international source
-# B = Established news media
-# C = Sports / specialized media
+TRANSFER_TERMS = [
+    "انتقال",
+    "انتقالات",
+    "صفقة",
+    "توقيع",
+    "يوقع",
+    "وقع",
+    "ضم",
+    "إعارة",
+    "إعارة",
+    "مفاوضات",
+    "اهتمام",
+    "عرض",
+    "عقد",
+    "تجديد",
+    "transfer",
+    "transfers",
+    "signing",
+    "signed",
+    "loan",
+    "contract",
+    "renewal",
+    "negotiation",
+    "interest",
+    "offer",
+]
 
+MOROCCAN_PLAYERS = [
+    "أشرف حكيمي",
+    "Achraf Hakimi",
+    "براهيم دياز",
+    "Brahim Diaz",
+    "يوسف النصيري",
+    "Youssef En-Nesyri",
+    "أيوب الكعبي",
+    "Ayoub El Kaabi",
+    "سفيان رحيمي",
+    "Soufiane Rahimi",
+    "نايف أكرد",
+    "Nayef Aguerd",
+    "عز الدين أوناحي",
+    "Azzedine Ounahi",
+    "حكيم زياش",
+    "Hakim Ziyech",
+    "سفيان أمرابط",
+    "Sofyan Amrabat",
+    "نصير مزراوي",
+    "Noussair Mazraoui",
+    "بلال الخنوس",
+    "Bilal El Khannouss",
+    "إلياس بن صغير",
+    "Eliesse Ben Seghir",
+    "عبد الصمد الزلزولي",
+    "Abde Ezzalzouli",
+    "أمين حارث",
+    "Amine Harit",
+    "ياسين بونو",
+    "Yassine Bounou",
+    "إسماعيل الصيباري",
+    "Ismael Saibari",
+    "رضا بلحيان",
+    "Reda Belahyane",
+    "أمين عدلي",
+    "Amine Adli",
+    "شادي رياض",
+    "Chadi Riad",
+    "إسماعيل قندوس",
+    "Ismael Kandouss",
+    "سليم أملاح",
+    "Selim Amallah",
+]
 
-# =========================================================
-# NEWS SOURCES
-# =========================================================
 
 SOURCES = [
 
-    # =====================================================
-    # 🇲🇦 MOROCCO
-    # =====================================================
-
+    # 🇲🇦 المغرب
     (
         "MAP عربي",
         "https://news.google.com/rss/search?q=site%3Amapnews.ma&hl=ar&gl=MA&ceid=MA%3Ama",
         "A"
     ),
-
     (
         "SNRTnews عربي",
         "https://news.google.com/rss/search?q=site%3Asnrtnews.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "A"
     ),
-
     (
         "هسبريس",
         "https://news.google.com/rss/search?q=site%3Ahespress.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "B"
     ),
-
     (
         "Le360 عربي",
         "https://news.google.com/rss/search?q=site%3Aar.le360.ma&hl=ar&gl=MA&ceid=MA%3Ama",
         "B"
     ),
-
     (
         "العمق المغربي",
         "https://news.google.com/rss/search?q=site%3Aal3omk.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "B"
     ),
-
     (
         "اليوم24",
         "https://news.google.com/rss/search?q=site%3Aalyaoum24.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "B"
     ),
-
     (
         "أخبارنا المغربية",
         "https://news.google.com/rss/search?q=site%3Aakhbarona.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "B"
     ),
-
     (
         "هبة بريس",
         "https://news.google.com/rss/search?q=site%3Ahibapress.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "B"
     ),
-
     (
         "برلمان",
         "https://news.google.com/rss/search?q=site%3Abarlamane.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "B"
     ),
-
     (
         "كود",
         "https://news.google.com/rss/search?q=site%3Agoud.ma&hl=ar&gl=MA&ceid=MA%3Ama",
         "B"
     ),
-
     (
         "كفاش",
         "https://news.google.com/rss/search?q=site%3Akifache.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "B"
     ),
-
     (
         "فبراير",
         "https://news.google.com/rss/search?q=site%3Afebrayer.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "B"
     ),
-
     (
         "البطولة",
         "https://news.google.com/rss/search?q=site%3Aelbotola.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "C"
     ),
-
     (
         "المنتخب",
         "https://news.google.com/rss/search?q=site%3Aalmountakhab.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "C"
     ),
 
-
-    # =====================================================
-    # 🌍 MIDDLE EAST
-    # =====================================================
-
+    # 🌍 العالم بالعربية
     (
         "الجزيرة",
         "https://news.google.com/rss/search?q=site%3Aaljazeera.net&hl=ar&gl=MA&ceid=MA%3Ama",
         "A"
     ),
-
     (
         "العربية",
         "https://news.google.com/rss/search?q=site%3Aalarabiya.net&hl=ar&gl=MA&ceid=MA%3Ama",
         "A"
     ),
-
     (
         "سكاي نيوز عربية",
         "https://news.google.com/rss/search?q=site%3Askynewsarabia.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "A"
     ),
-
     (
         "الشرق للأخبار",
         "https://news.google.com/rss/search?q=site%3Aasharq.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "A"
     ),
-
     (
         "الشرق الأوسط",
         "https://news.google.com/rss/search?q=site%3Aaawsat.com&hl=ar&gl=MA&ceid=MA%3Ama",
         "A"
     ),
-
     (
         "العربي الجديد",
         "https://news.google.com/rss/search?q=site%3Aalaraby.co.uk&hl=ar&gl=MA&ceid=MA%3Ama",
         "B"
     ),
+    (
+        "القدس العربي",
+        "https://news.google.com/rss/search?q=site%3Aalquds.co.uk&hl=ar&gl=MA&ceid=MA%3Ama",
+        "B"
+    ),
+    (
+        "فرانس 24 عربي",
+        "https://news.google.com/rss/search?q=site%3Afrance24.com+arabic&hl=ar&gl=MA&ceid=MA%3Ama",
+        "A"
+    ),
+    (
+        "DW عربية",
+        "https://news.google.com/rss/search?q=site%3Adw.com+arabic&hl=ar&gl=MA&ceid=MA%3Ama",
+        "A"
+    ),
+    (
+        "BBC عربي",
+        "https://news.google.com/rss/search?q=site%3Abbc.com+arabic&hl=ar&gl=MA&ceid=MA%3Ama",
+        "A"
+    ),
+    (
+        "يورونيوز عربي",
+        "https://news.google.com/rss/search?q=site%3Aeuronews.com+arabic&hl=ar&gl=MA&ceid=MA%3Ama",
+        "B"
+    ),
+    (
+        "إندبندنت عربية",
+        "https://news.google.com/rss/search?q=site%3Aindependentarabia.com&hl=ar&gl=MA&ceid=MA%3Ama",
+        "B"
+    ),
+    (
+        "CNN بالعربية",
+        "https://news.google.com/rss/search?q=site%3Aarabic.cnn.com&hl=ar&gl=MA&ceid=MA%3Ama",
+        "A"
+    ),
 
-
-    # =====================================================
-    # 🌐 INTERNATIONAL
-    # =====================================================
-
+    # 🔎 مصادر دولية للتحقق والرصد
     (
         "Reuters",
         "https://news.google.com/rss/search?q=site%3Areuters.com&hl=en&gl=US&ceid=US%3Aen",
         "A"
     ),
-
-    (
-        "BBC",
-        "https://news.google.com/rss/search?q=site%3Abbc.com&hl=en&gl=US&ceid=US%3Aen",
-        "A"
-    ),
-
     (
         "Associated Press",
         "https://news.google.com/rss/search?q=site%3Aapnews.com&hl=en&gl=US&ceid=US%3Aen",
         "A"
     ),
-
-    (
-        "France 24 عربي",
-        "https://news.google.com/rss/search?q=site%3Afrance24.com+arabic&hl=ar&gl=MA&ceid=MA%3Ama",
-        "A"
-    ),
-
-    (
-        "DW عربية",
-        "https://news.google.com/rss/search?q=site%3Adw.com+arabic&hl=ar&gl=MA&ceid=MA%3Ama",
-        "A"
-    )
-]
-
-
-# =========================================================
-# 🇲🇦 MOROCCO KEYWORDS
-# =========================================================
-
-MOROCCO_TERMS = [
-
-    "المغرب",
-    "المغربي",
-    "المغربية",
-    "الرباط",
-    "الدار البيضاء",
-    "الدارالبيضاء",
-    "مراكش",
-    "طنجة",
-    "فاس",
-    "مكناس",
-    "أكادير",
-    "وجدة",
-    "تطوان",
-    "الحسيمة",
-    "الناظور",
-    "القنيطرة",
-    "سلا",
-    "الجديدة",
-    "آسفي",
-    "بني ملال",
-    "الرشيدية",
-    "ورزازات",
-    "الصويرة",
-    "العيون",
-    "الداخلة",
-    "البرلمان المغربي",
-    "الحكومة المغربية",
-    "الملك محمد السادس",
-    "رئيس الحكومة",
-    "وزارة الداخلية المغربية",
-    "وزارة الخارجية المغربية",
-    "الاقتصاد المغربي",
-    "المنتخب المغربي",
-    "أسود الأطلس",
-    "المغرب العربي",
-    "Morocco",
-    "Moroccan",
-    "Maroc",
-    "Marocain",
-    "Marocaine"
-]
-
-
-# =========================================================
-# 🔄 TRANSFER KEYWORDS
-# =========================================================
-
-TRANSFER_TERMS = [
-
-    "انتقال",
-    "انتقالات",
-    "انتقل",
-    "ينتقل",
-    "انتقاله",
-    "تعاقد",
-    "يتعاقد",
-    "تعاقده",
-    "وقع",
-    "يوقع",
-    "توقيع",
-    "صفقة",
-    "عرض",
-    "عروض",
-    "مفاوضات",
-    "مفاوضات",
-    "اهتمام",
-    "يرغب",
-    "إعارة",
-    "إعارة",
-    "تجديد",
-    "يجدد",
-    "رحيل",
-    "يرحل",
-    "غادر",
-    "ينضم",
-    "انضم",
-    "ناديه الجديد",
-    "free agent",
-    "transfer",
-    "transfers",
-    "signed",
-    "signing",
-    "joins",
-    "joined",
-    "loan",
-    "contract",
-    "renewal",
-    "negotiations",
-    "interest",
-    "offer"
-]
-
-
-# =========================================================
-# ⚽ MOROCCAN PLAYERS
-# =====================================================
-
-MOROCCAN_PLAYERS = [
-
-    "أشرف حكيمي",
-    "حكيمي",
-    "Achraf Hakimi",
-
-    "براهيم دياز",
-    "براهيم دياز",
-    "Brahim Diaz",
-    "Brahim Díaz",
-
-    "يوسف النصيري",
-    "النصيري",
-    "Youssef En-Nesyri",
-
-    "أيوب الكعبي",
-    "الكعبي",
-    "Ayoub El Kaabi",
-
-    "سفيان رحيمي",
-    "رحيمي",
-    "Soufiane Rahimi",
-
-    "إبراهيم دياز",
-    "إبراهيم دياز",
-
-    "نايف أكرد",
-    "أكرد",
-    "Nayef Aguerd",
-
-    "عز الدين أوناحي",
-    "أوناحي",
-    "Azzedine Ounahi",
-
-    "حكيم زياش",
-    "زياش",
-    "Hakim Ziyech",
-
-    "سفيان أمرابط",
-    "أمرابط",
-    "Sofyan Amrabat",
-
-    "نور الدين أمرابط",
-    "Nordin Amrabat",
-
-    "بلال الخنوس",
-    "الخنوس",
-    "Bilal El Khannouss",
-
-    "إلياس بن صغير",
-    "بن صغير",
-    "Eliesse Ben Seghir",
-
-    "عبد الصمد الزلزولي",
-    "الزلزولي",
-    "Abde Ezzalzouli",
-
-    "أمين حارث",
-    "حارث",
-    "Amine Harit",
-
-    "منير المحمدي",
-    "المحمدي",
-    "Munir El Kajoui",
-
-    "ياسين بونو",
-    "بونو",
-    "Yassine Bounou",
-
-    "منير الحدادي",
-    "الحدادي",
-    "Munir El Haddadi",
-
-    "سليم أملاح",
-    "أملاح",
-    "Selim Amallah",
-
-    "عادل تاعرابت",
-    "تاعرابت",
-    "Adel Taarabt",
-
-    "يوسف آيت بن ناصر",
-    "بن ناصر",
-    "Youssef Aït Bennasser",
-
-    "رضا بلحيان",
-    "بلحيان",
-    "Reda Belahyane",
-
-    "أسامة العزوزي",
-    "العزوزي",
-    "Oussama El Azzouzi",
-
-    "إسماعيل الصيباري",
-    "الصيباري",
-    "Ismael Saibari",
-
-    "عبد الكبير عبقار",
-    "عبقار",
-    "Abdelkabir Abqar",
-
-    "شادي رياض",
-    "رياض",
-    "Chadi Riad",
-
-    "محمد الشيبي",
-    "الشيبي",
-    "Mohamed Chibi",
-
-    "نصير مزراوي",
-    "مزراوي",
-    "Noussair Mazraoui",
-
-    "أمين عدلي",
-    "عدلي",
-    "Amine Adli",
-
-    "إسحاق الحداد",
-    "Ismail Kandouss",
-    "إسماعيل قندوس"
 ]
